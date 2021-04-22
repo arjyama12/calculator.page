cbe02808199f639f7d16fab620a2cc047be24f10
arjyama seal <arjyama12@gmail.com>
2021-04-22 15:22:39 +0530

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+function dis(val){
+    document.getElementById("result").value+=val;
+    console.log(val);
+    }
+    function solve(){
+        var x=document.getElementById("result").value;
+        var y=eval(x);
+        var y=eval(Math.sin(90*Math.PI/180));
+        document.getElementById("result").value=y;
+    }
+    function clr()
+    {
+        document.getElementById("result").value="";
+    }
+    function sin{
+        calc.txt.value=Math.sin(calc.txt.value);
+    }
+    
